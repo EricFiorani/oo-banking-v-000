71cc9e23312aca8ef8f283a1801ce8201444d026
Eric Fiorani <efiorani305@gmail.com>
2018-05-16 18:07:05 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,4 +14,13 @@ class Transfer
     sender.valid? && receiver.valid? ? true : false
   end
 
+  def execute_transaction
+    if self.valid? && self.status != "complete" && @sender.balance > @amount
+      @sender.balance -= @amount && @receiver.balance += @amount
+      return self.status = "complete"
+    else
+      self.reject_transfer
+    end
+  end
+
 end
